e106a5c66a14008eb9d50beae1a1f334781d6196
Bystroushaak <bystrousak@kitakitsune.org>
2015-07-17 17:16:18 +0200

    getVersion() added to setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@
 #
 # Imports =====================================================================
 from setuptools import setup, find_packages
-from docs import getVersion
 
 
 # Variables ===================================================================
@@ -14,6 +13,26 @@ long_description = "\n\n".join([
     open('README.rst').read(),
     changelog
 ])
+
+
+# Functions ===================================================================
+def allSame(s):
+    return not any(filter(lambda x: x != s[0], s))
+
+
+def hasDigit(s):
+    return any(char.isdigit() for char in s)
+
+
+def getVersion(data):
+    """
+    Parse version from changelog written in RST format.
+    """
+    data = data.splitlines()
+    return next((
+        v for v, u in zip(data, data[1:])  # v = version, u = underline
+        if len(v) == len(u) and allSame(u) and hasDigit(v) and "." in v
+    ))
 
 
 # Actual setup definition =====================================================
